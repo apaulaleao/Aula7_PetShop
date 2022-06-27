1161e7dfa9dba29e1691ec744025b2333ecac468
Ana Paula Leão <paula.leao@gmail.com>
2022-06-27 18:45:18 -0300

    Adicionar arquivos de projeto.

---
+++ Petshop.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/*cadastro de animais de uma pet-shop;. 
+ * Crie uma classe que modele os animais e contenha:
+ * atributos nome, espécie, cor, idade, observação e dados do dono. 
+ * Os atributos relacionados ao dono do animal deve ser (privados) para proteção de dados. 
+ * Forneça métodos para cadastro e leitura das informações.*/
+
+namespace PetShop
+{
+    internal class Petshop
+    {
+        public string nomePet { get; set; }
+        public string especie { get; set; }
+        public string cor {get;set;}
+        public int idade { get; set; }
+        public string observacao { get; set; }
+
+        private string nomeDono { get; set; }
+        private string addressDono { get; set; }
+        private double whatsapp_dono { get; set; }
+
+        public Petshop(string nomePet, string especie, string cor, int idade, string observacao, string nomeDono, string addressDono, double whatsapp_dono)
+        {
+            this.nomePet = nomePet;
+            this.especie = especie;
+            this.cor = cor;
+            this.idade = idade;
+            this.observacao = observacao;
+            this.nomeDono = nomeDono;
+            this.addressDono = addressDono;
+            this.whatsapp_dono = whatsapp_dono;
+        }
+        public override string ToString()
+        {
+            return ($"O animal é um {nomePet}/n, do tipo{especie},/n, de cor{cor}");
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Modulo_PetShop
+{
+
+    internal class PetShop
+    {
+        private string nomePet;
+        private string especie;
+        private string cor;
+        private int idade;
+        private string observacao;
+
+        
+        public PetShop(string nomePet, string especie, string cor, int idade, string observacao) : this(nomePet)
+        {
+            this.nomePet = nomePet;
+            this.especie = especie;
+            this.cor = cor;
+            this.idade = idade;
+            this.observacao = observacao;
+        }
+
+        static void Main(string[] args)
+        {
+
+            Console.Write("Nome do pet:");
+            string nomePet = Console.ReadLine();
+
+            Console.Write("Qual a espécie do Pet:");
+            string especie = Console.ReadLine();
+
+            Console.Write("Qual a cor do pet:");
+            string cor = Console.ReadLine();
+
+            Console.Write("Qual a idade do pet:");
+            int idade = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Campo de Observação:");
+            string observacao = Console.ReadLine();
+
+            PetShop petShop1 = new PetShop(nomePet, especie, cor, idade, observacao);
+            Console.WriteLine();
+            Console.WriteLine(petShop1);
+
+
+
+        }
+    }
+}
